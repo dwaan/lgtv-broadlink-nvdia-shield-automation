a8c4e7824efdfebc8a34eb5ec82a3608ea5b188c
Dwan <me@dwaan.com>
2020-06-21 11:07:00 +0300

    Create shield.sh
    Add script to detect is shield is wake up

---
+++ shield.sh
@@ -0,0 +1,19 @@
+if [ ! -d ~/.homebridgestates ]
+then
+    mkdir ~/.homebridgestates
+fi
+
+if [ $1 = "keyevent" ]
+then
+        adb shell input keyevent $2
+elif [ $1 = "wake" ]
+then
+        adb shell input keyevent KEYCODE_WAKEUP
+elif [ $1 = "sleep" ]
+then
+        adb shell input keyevent KEYCODE_SLEEP
+elif [ $1 = "sleepstatus" ]
+then
+        status=$(adb shell "dumpsys power | grep mHoldingDisplay  | cut -d = -f 2");
+        echo $status
+fi
